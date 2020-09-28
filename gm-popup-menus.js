cf3c3ab1e8782ae37254ede272d1363151884632
Sloane Fox <SloaneFox@gmx.com>
2020-09-29 00:24:29 +0100

    update

--- gm-popup-menus.js
+++ gm-popup-menus.js
@@ -98,7 +98,7 @@ var GM_registerMenuCommand;           //Uses closure to ensure a different funct
 var submenuModule=(function() { try {  // a module, js pattern module, returns interfaceObj.  ownSubmenu() is a closure returning
 	// an interface object in scope of 'this'.  Side effect alters GM_registerMenuCommand.  Not window.submenuModule clash of multiusage.
 	var sify=JSON.stringify, ownSubmenu, ownSubmenuList, xbutton, body, state=null;
-	var coord_id=1, $, nlist=1, right_pos=true, scriptName, nofocus, altHotkey=77, thishere, list_orig_height, chromeButton, queue;
+	var coord_id=1, $, nlist=1, right_pos="truthy", scriptName, nofocus, altHotkey=77, thishere, list_orig_height, chromeButton, queue;
 	var regmutex, osmlisel="li.osm-button",lis, plat_chrome=/Chrome/.test(navigator.userAgent), uw=unsafeWindow, cmd, ln="\u2501", menuwrap, shrink_factor, header;
 	var iframe=window!=window.parent, lmarg=window.innerWidth*0.04, blank_textContent=false;    //77==m, 0.04==4%.
 	var init=async function(script_name, hotkey, title_color, itsBackgroundColor, dont_focus) //is submenuModule.register() 
